2491dd1df89133bd883c7c9982c7ef4c882962f9
lutfiqaraman <lutfiqaraman@gmail.com>
2020-05-30 00:45:59 +0300

    create a dbcontext

---
+++ backend/PaymentAPI/Models/PaymentDetailContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace PaymentAPI.Models
+{
+  public class PaymentDetailContext : DbContext
+  {
+    public PaymentDetailContext(DbContextOptions<PaymentDetailContext> options) : base(options)
+    {
+
+    }
+  }
+}
