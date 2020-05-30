0907082f5b47171c6c214ce6d41166cc46b78bf0
lutfiqaraman <lutfiqaraman@gmail.com>
2020-05-30 14:06:57 +0300

    write the CRUD operations

--- backend/PaymentAPI/Controllers/PaymentDetailController.cs
+++ backend/PaymentAPI/Controllers/PaymentDetailController.cs
@@ -1,4 +1,8 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using PaymentAPI.Models;
 
 namespace PaymentAPI.Controllers
@@ -13,5 +17,63 @@ namespace PaymentAPI.Controllers
     {
       _context = context;
     }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<PaymentDetail>>> GetPaymentDetails()
+    {
+      return await _context.PaymentDetails.ToListAsync();
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutPaymentDetail(int id, PaymentDetail paymentDetail)
+    {
+      if (id != paymentDetail.PMId)
+        return BadRequest();
+
+      _context.Entry(paymentDetail).State = EntityState.Modified;
+
+      try
+      {
+        await _context.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        if (PaymentDetailExists(id))
+          return NotFound();
+        else
+          throw;
+      }
+
+      return NoContent();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<PaymentDetail>> PostPaymentDetail(PaymentDetail paymentDetail)
+    {
+      _context.PaymentDetails.Add(paymentDetail);
+      await _context.SaveChangesAsync();
+
+      return CreatedAtAction("GetPaymentDetails", new { id = paymentDetail.PMId }, paymentDetail);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult<PaymentDetail>> DeletePaymentDetail(int id)
+    {
+      PaymentDetail paymentDetail = await _context.PaymentDetails.FindAsync(id);
+
+      if (paymentDetail == null)
+        return NotFound();
+      
+      _context.PaymentDetails.Remove(paymentDetail);
+      await _context.SaveChangesAsync();
+
+      return paymentDetail;
+    }
+
+    private bool PaymentDetailExists(int id)
+    {
+      return _context.PaymentDetails.Any(e => e.PMId == id);
+    }
+
   }
 }
