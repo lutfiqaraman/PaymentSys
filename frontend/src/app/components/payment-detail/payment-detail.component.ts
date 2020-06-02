380565e1d4e35f5e25baf521206ea7f8125877cb
lutfiqaraman <lutfiqaraman@gmail.com>
2020-06-02 10:45:54 +0300

    add alert msg to the component

--- frontend/src/app/components/payment-detail/payment-detail.component.ts
+++ frontend/src/app/components/payment-detail/payment-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PaymentDetailService } from 'src/app/shared/payment-detail.service';
 import { NgForm } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-payment-detail',
@@ -8,7 +9,10 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./payment-detail.component.css'],
 })
 export class PaymentDetailComponent implements OnInit {
-  constructor(public paymentService: PaymentDetailService) {}
+  constructor(
+    public paymentService: PaymentDetailService,
+    private alertMsg: ToastrService
+  ) {}
 
   ngOnInit(): void {
     this.restForm();
@@ -32,6 +36,7 @@ export class PaymentDetailComponent implements OnInit {
     this.paymentService.postPaymentDetail(form.value).subscribe(
       () => {
         this.restForm(form);
+        this.alertMsg.success('Submitted successfully', 'Payment Register');
       },
       (err) => {
         console.log(err);
